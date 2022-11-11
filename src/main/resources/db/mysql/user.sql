bcd64ac086f0d653df8ad3f53b352c502e1e0c05
Oleksandr Tsyhankov <oleksandr_tsyhankov@epam.com>
2022-11-11 09:28:39 +0200

    user.sql 5.7

--- src/main/resources/db/mysql/user.sql
+++ src/main/resources/db/mysql/user.sql
@@ -1,12 +1,12 @@
-CREATE DATABASE petclinic;
-CREATE USER petclinic@'%' IDENTIFIED BY 'petclinic';
-GRANT ALL PRIVILEGES ON petclinic.* TO 'petclinic'@'%';
-FLUSH PRIVILEGES;
+-- CREATE DATABASE petclinic;
+-- CREATE USER petclinic@'%' IDENTIFIED BY 'petclinic';
+-- GRANT ALL PRIVILEGES ON petclinic.* TO 'petclinic'@'%';
+-- FLUSH PRIVILEGES;
 
--- CREATE DATABASE IF NOT EXISTS petclinic;
+CREATE DATABASE IF NOT EXISTS petclinic;
 
--- ALTER DATABASE petclinic
---   DEFAULT CHARACTER SET utf8
---   DEFAULT COLLATE utf8_general_ci;
+ALTER DATABASE petclinic
+  DEFAULT CHARACTER SET utf8
+  DEFAULT COLLATE utf8_general_ci;
 
--- GRANT ALL PRIVILEGES ON petclinic.* TO 'petclinic'@'%' IDENTIFIED BY 'petclinic';
+GRANT ALL PRIVILEGES ON petclinic.* TO 'petclinic'@'%' IDENTIFIED BY 'petclinic';
